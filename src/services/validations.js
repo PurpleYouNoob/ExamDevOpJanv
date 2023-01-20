c1712c82ebf381603af9fcbc20e304f15c249380
sasha_tour <sashavanrossum9@gmail.com>
2023-01-20 11:04:12 +0100

    fix(app): prettier fix

--- src/services/validations.js
+++ src/services/validations.js
@@ -3,7 +3,8 @@
 var format = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
 const isEmpty = (label) => !label || label.length === 0;
-const isValid = (label) => label.length >= 8 && format.test(label) && /\d/.test(label);
+const isValid = (label) =>
+    label.length >= 8 && format.test(label) && /\d/.test(label);
 
 exports.isEmpty = isEmpty;
 exports.isValid = isValid;
