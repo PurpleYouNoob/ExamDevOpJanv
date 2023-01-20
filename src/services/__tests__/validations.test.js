03a8942a9ac00b4460876e8093c7aa0df87731d8
sasha_tour <sashavanrossum9@gmail.com>
2023-01-20 10:51:58 +0100

    test(services): check if valid gamer tag defenition and first test

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,4 +1,4 @@
-const { isEmpty } = require("../validations");
+const { isEmpty, isValid } = require("../validations");
 
 describe("validations tests suites - isEmpty", () => {
     test("should return true as the label is undefined", () => {
@@ -18,3 +18,10 @@ describe("validations tests suites - isEmpty", () => {
 });
 
 // TODO: Create tests suite for validation function
+
+describe("validations tests suites - isValid", () => {
+    it("should return false as the label less then 8 char", () => {
+        const result = isValid("Lo");
+        expect(result).toBe(false);
+    });
+});
--- src/services/validations.js
+++ src/services/validations.js
@@ -1,5 +1,7 @@
 // TODO: Add here the function to validate
 
 const isEmpty = (label) => !label || label.length === 0;
+const isValid = (label) => label.length >= 8 ;
 
 exports.isEmpty = isEmpty;
+exports.isValid = isValid;
